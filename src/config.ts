e232b4ffacf5c9d69c9548dfa1a20ea39dc12827
cmtradepro <admin@cmtrade.pro>
2020-10-12 21:48:47 +0800

    http

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export default {
-  rpc: process.env.NODE_ENV === 'production' ?
-    'https://mainnet.infura.io/v3/a0c696c1e5e848c08c2276db4dbca103' :
-    'https://mainnet.infura.io/v3/a0c696c1e5e848c08c2276db4dbca103',
+  rpc: process.env.NODE_ENV === 'production' ? 
+    'https://wallet.tomochain.com/api/luaswap/rpc' :
+    'https://wallet.tomochain.com/api/luaswap/rpc',
   chainId: 1,
-  api: process.env.NODE_ENV === 'production' ?
-  'https://mainnet.infura.io/v3/a0c696c1e5e848c08c2276db4dbca103' :
-  'https://mainnet.infura.io/v3/a0c696c1e5e848c08c2276db4dbca103'
+  api: process.env.NODE_ENV === 'production' ? 
+  'https://wallet.tomochain.com/api/luaswap' :
+  'https://wallet.tomochain.com/api/luaswap'
 }
